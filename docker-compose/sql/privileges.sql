e2bfc3765fd14b274294038102804c64dcf88d3d
Connor Zeng <ganggang334@gmail.com>
2021-08-17 14:26:06 +0800

    add redis manager

--- docker-compose/sql/privileges.sql
+++ docker-compose/sql/privileges.sql
@@ -6,5 +6,13 @@ create user nacosopr identified by 'nacosopr';
 grant all on nacos.* to nacosopr@'%' identified by 'nacosopr' with grant option;
 
 
+-- 这个中间件没什么用
+-- redis manager
+-- create user redis_manager_orp identified by 'redis_manager_orp';
+-- grant all on redis_manager.* to redis_manager_orp@'%' identified by 'redis_manager_orp' with grant option;
+
+
+
+
 -- 这一条命令一定要有：
 flush privileges;
---
+++ docker-compose/sql/schema-redis-manager.sql
@@ -0,0 +1,5 @@
+
+-- 创建数据库
+-- create database `redis_manager` default character set utf8 collate utf8_general_ci;
+
+
