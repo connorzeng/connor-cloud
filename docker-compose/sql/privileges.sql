aed3d703e0734bacd68f74fc910f314455faa26e
Connor Zeng <ganggang334@gmail.com>
2021-08-17 14:27:52 +0800

    remove redis manager

--- docker-compose/sql/privileges.sql
+++ docker-compose/sql/privileges.sql
@@ -6,12 +6,6 @@ create user nacosopr identified by 'nacosopr';
 grant all on nacos.* to nacosopr@'%' identified by 'nacosopr' with grant option;
 
 
--- 这个中间件没什么用
--- redis manager
--- create user redis_manager_orp identified by 'redis_manager_orp';
--- grant all on redis_manager.* to redis_manager_orp@'%' identified by 'redis_manager_orp' with grant option;
-
-
 
 
 -- 这一条命令一定要有：
--- docker-compose/sql/schema-redis-manager.sql
+++
@@ -1,5 +0,0 @@
-
--- 创建数据库
--- create database `redis_manager` default character set utf8 collate utf8_general_ci;
-
-
